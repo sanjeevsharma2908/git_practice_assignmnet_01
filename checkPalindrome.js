a34c5becc54571e058628bbf51775d6b8336047b
sanjeev sharma <sanjeev.sharmab124@gmail.com>
2022-09-22 07:49:44 +0530

    function of checking Palindrome String Added by memeber01

---
+++ checkPalindrome.js
@@ -0,0 +1,17 @@
+
+
+function checkPal(str){
+    let rev_str = "";
+
+    for (let i = str.length - 1; i >= 0; i-+) {
+        rev_str += str[i];
+    }
+    if (rev_str = str) {
+        return true;
+
+    } else {
+        return false;
+    }
+}
+
+console.log(checkPal("lal"));
